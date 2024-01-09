adde68321933bbe33b22448ab612e310e3535c4e
jurayevcoder <davlatjurayev.1@gmail.com>
2024-01-09 16:57:00 +0500

    ok

--- components/Header.vue
+++ components/Header.vue
@@ -9,22 +9,22 @@
                 </div>
                 <div>
                     <ul class="flex items-center gap-8 text-[18px] text-white font-['Poppins'] font-light">
-                        <li>
+                        <li class="hover:font-bold">
                             <NuxtLink to="/">Home</NuxtLink>
                         </li>
-                        <li>
+                        <li class="hover:font-bold">
                             <NuxtLink to="about">About</NuxtLink>
                         </li>
-                        <li>
+                        <li class="hover:font-bold">
                             <NuxtLink to="service">Service</NuxtLink>
                         </li>
-                        <li>
+                        <li class="hover:font-bold">
                             <NuxtLink to="contact">Contact</NuxtLink>
                         </li>
                         <li class="pl-5 border-l py-5">
                             <div class="relative">
-                                <button @click="modallang" class="flex items-end text-white font-['Poppins'] font-light cursor-pointer">Eng <i :class="lang ? 'bx bx-chevron-up text-[#1976DC]' : 'bx bx-chevron-down text-[#1976DC]'"></i></button>
-                                <div :class="lang ? 'absolute left-0 top-8 bg-[#323459] flex flex-col' : 'hidden'">
+                                <button @click="modallang" class="flex items-end text-white hover:font-bold font-['Poppins'] font-light cursor-pointer">Eng <i :class="lang ? 'bx bx-chevron-up text-[#1976DC]' : 'bx bx-chevron-down text-[#1976DC]'"></i></button>
+                                <div :class="lang ? 'absolute z-10 left-0 top-8 bg-[#323459] flex flex-col' : 'hidden'">
                                     <button class="bg-transparent text-white font-['Poppins'] font-light px-5 py-3">Rus</button>
                                     <button class="bg-transparent text-white font-['Poppins'] font-light px-5 py-3">Uzb</button>
                                 </div>
@@ -32,15 +32,17 @@
                         </li>
                         <li>
                             <div class="relative">
-                                <span class="text-[#1976DC] text-[25px] cursor-pointer flex items-center">
+                                <button @click="modalbell" class="text-[#1976DC] text-[25px] cursor-pointer flex items-center">
                                     <i class='bx bxs-bell flex items-start' >
                                         <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" fill="none">
                                             <circle cx="4" cy="4" r="4" fill="#E76514"/>
                                         </svg>
                                     </i>    
-                                </span>
-                                <div class="hidden absolute bg-[#323459] flex flex-col">
-                                    <a href="#" class="p-5">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</a>
+                                </button>
+                                <div :class="bell ? 'absolute right-0 top-8 bg-[#323459] flex flex-col' : 'hidden'">
+                                    <a href="#" class="p-5 w-[300px]">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</a>
+                                    <hr>
+                                    <a href="#" class="p-5 w-[300px]">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</a>
                                 </div>
                             </div>
                         </li>
@@ -55,6 +57,9 @@
 import { ref } from 'vue';
 const lang = ref(false);
 const modallang = () => (lang.value = !lang.value);
+
+const bell = ref(false);
+const modalbell = () => (bell.value = !bell.value);
 console.log();
 </script>
 
