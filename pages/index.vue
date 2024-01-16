393e04a54f5eaec031b6c96de4f75de9deee3edf
jurayevcoder <davlatjurayev.1@gmail.com>
2024-01-16 16:26:20 +0500

    about language

--- pages/index.vue
+++ pages/index.vue
@@ -19,8 +19,6 @@
           <a href="tel:+998 90 555 55 56">{{ $t("Hero-btn") }}</a>
         </button>
         <a
-          data-aos="fade-up"
-          data-aos-duration="2000"
           href="tel:+998 90 555 55 56"
           class="sm:hidden flex items-center gap-3 text-white border-2 border-[#1976DC] rounded-md w-[198px] font-light font-['Poppins'] px-2 py-1"
         >
@@ -622,13 +620,17 @@
           </h4>
           <div class="narx__card">
             <h3>
-              <img src="/home/ximoya--icon.png" alt="icon" />{{ $t("section7-subtitle1") }}
+              <img src="/home/ximoya--icon.png" alt="icon" />{{
+                $t("section7-subtitle1")
+              }}
             </h3>
             <h3>
-              <img src="/home/ximoya--icon.png" alt="icon" /> {{ $t("section7-subtitle2") }}
+              <img src="/home/ximoya--icon.png" alt="icon" />
+              {{ $t("section7-subtitle2") }}
             </h3>
             <h3>
-              <img src="/home/ximoya--icon.png" alt="icon" /> {{ $t("section7-subtitle3") }}
+              <img src="/home/ximoya--icon.png" alt="icon" />
+              {{ $t("section7-subtitle3") }}
             </h3>
           </div>
           <button>{{ $t("section7-btn") }}</button>
@@ -723,19 +725,27 @@
   <section class="form">
     <div class="container mx-auto p-5">
       <div class="form__wrapper">
-        <form
-          class="form2 sm:w-[500px]"
-        >
+        <form class="form2 sm:w-[500px]">
           <span id="form__h1"> {{ $t("section8-title") }} </span>
-          <label for="form__inp" >Ism familya *</label><input   class="outline-none"  required style="" id="form__inp" type="text" >
-           <label for=" tel">Telefon raqamingiz</label><input id="tel"  class="outline-none" required type="text" >
-            <label for="Xizmat">Xizmat turi * </label><input id="Xizmat" class="outline-none" required type="text"  >
-            <label for="Maqsadingiz">Maqsadingiz *</label><input id="Maqsadingiz" class="outline-none" required type="text" >
-          <button required class="form__btn" type="submit">{{ $t("section8-btn") }}</button>I
+          <label for="form__inp">{{ $t("section8-form-name") }}</label
+          ><input
+            class="outline-none"
+            required
+            style=""
+            id="form__inp"
+            type="text"
+          />
+          <label for="tel">{{ $t("section8-form-tel") }}</label
+          ><input id="tel" class="outline-none" required type="text" />
+          <label for="Xizmat">{{ $t("section8-form-xizmat") }}</label
+          ><input id="Xizmat" class="outline-none" required type="text" />
+          <label for="Maqsadingiz">{{ $t("section8-form-maqsad") }}</label
+          ><input id="Maqsadingiz" class="outline-none" required type="text" />
+          <button required class="form__btn" type="submit">
+            {{ $t("section8-btn") }}</button
+          >I
         </form>
-        <div
-          class="lg:flex flex-col items-center hidden"
-        >
+        <div class="lg:flex flex-col items-center hidden">
           <img src="/about_img/form__img1.png" alt="foto" />
           <img src="/about_img/form__ten.png" alt="foto" />
         </div>
